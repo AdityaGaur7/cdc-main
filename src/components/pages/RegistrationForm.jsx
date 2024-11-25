da8196809782d83103f0b40dc6da36791b7ab610
LAZY-CODER01 <avinashmaurya430@gmail.com>
2024-11-25 23:02:40 +0530

    main

--- src/components/pages/RegistrationForm.jsx
+++ src/components/pages/RegistrationForm.jsx
@@ -25,7 +25,7 @@ const RegistrationForm = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:5000/api/auth/signup', {
+      const response = await fetch('https://cdc-finalserver.vercel.app/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
